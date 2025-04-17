18478dc9b1e7e9e6232cacb4b558c4e8244ecb44
cesaraa09 <117768902+cesaraa09@users.noreply.github.com>
2025-04-17 19:48:28 -0300

    Update Main.java

--- src/engsoft/jogo/patos/Main.java
+++ src/engsoft/jogo/patos/Main.java
@@ -3,17 +3,17 @@ package engsoft.jogo.patos;
 public class Main {
 
 	public static void main(String[] args) {
-		Pato pt = new Pato_Ruivo();		
+		Pato pt = new Pato_Ruivo(); // Código em tempo de execução, instanciando um objeto concreto		
 		
-		System.out.println(pt.mostrar());
+		System.out.println(pt.mostrar()); 
 		System.out.println(pt.nadar());
 		System.out.println(pt.comportamento_pato());
 		
-		pt.setComportamento(new Voar_Foguete(), new Pular_Alto());
+		pt.setComportamento(new Voar_Foguete(), new Pular_Alto()); // Código em tempo de execução, mudando o comportamento de voo e *pulo 
 		
 		System.out.println(pt.comportamento_pato());
 		
-		pt.setComportamento(new Voar_Raso(), new Pular_Baixo());
+		pt.setComportamento(new Voar_Raso(), new Pular_Baixo()); // Código em tempo de execução, mudando o comportamento de voo e *pulo novamente
 		
 		System.out.println(pt.comportamento_pato());
 		
