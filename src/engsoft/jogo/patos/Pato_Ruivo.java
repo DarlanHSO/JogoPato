c561d76d8744d293f78c2b963acd7e55075b489e
cesaraa09 <117768902+cesaraa09@users.noreply.github.com>
2025-04-17 19:38:37 -0300

    Update Pato_Ruivo.java

--- src/engsoft/jogo/patos/Pato_Ruivo.java
+++ src/engsoft/jogo/patos/Pato_Ruivo.java
@@ -1,23 +1,23 @@
 package engsoft.jogo.patos;
 
-public class Pato_Ruivo extends Pato implements Padrao_Grasnar{
-
+public class Pato_Ruivo extends Pato implements Padrao_Grasnar{ // Classe concreta, Herança: Pato_Ruivo é uma classe filha de Pato,
+                                                                // e implementa a interface Padrao_Grasnar
 	public Pato_Ruivo()
 	{
-		setComportamento(new Voaveis_Asa(), new Pular_Pata());		
+		setComportamento(new Voaveis_Asa(), new Pular_Pata()); // Código em tempo de implementação, inicializando o comportamento de voo e **pulo (atividade 2)
 	}
 	
 	public String mostrar() {
-		return "Eu sou o Pato Ruivo." ;
+		return "Eu sou o Pato Ruivo." ; // Poliformismo: Cada pato diz algo diferente usando o método mostrar 
 	}
 
-	public String grasnar() {
+	public String grasnar() { // Poliformismo: Cada pato faz um som diferente usando o método grasnar 
 		// TODO Auto-generated method stub
 		return 	"Que-Que.";
 	
 	}
 
-	public String pular() {
+	public String pular() { // Poliformismo: Cada pato faz um som diferente usando o método pular  
 		return "Eu sei pular.";
 	}
 
