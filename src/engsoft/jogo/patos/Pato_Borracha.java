960b88e83cb3ba90f27f843d8f6966c786081043
cesaraa09 <117768902+cesaraa09@users.noreply.github.com>
2025-04-17 19:43:04 -0300

    Update Pato_Borracha.java

--- src/engsoft/jogo/patos/Pato_Borracha.java
+++ src/engsoft/jogo/patos/Pato_Borracha.java
@@ -1,14 +1,14 @@
 package engsoft.jogo.patos;
 
-public class Pato_Borracha extends Pato {
+public class Pato_Borracha extends Pato { // Classe concreta, Herança: Pato_Borracha é uma classe filha de Pato
 
 	public Pato_Borracha()
 	{
-		setComportamento(new Nao_Voa(), new Nao_Pula());	
+		setComportamento(new Nao_Voa(), new Nao_Pula());  // Código em tempo de implementação, inicializando o comportamento de voo e *não pulo	(atividade 2)
 	}
 	
 	public String mostrar() {
-		return "Ola, eu sou de Boarracha.";
+		return "Ola, eu sou de Boarracha."; // Poliformismo: Cada pato diz algo diferente usando o método mostrar 
 	}
 	
 }
